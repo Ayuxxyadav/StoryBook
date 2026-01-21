c0641bd895a059be712ed04160a65f743905c105
Ayush Yadav <ayushyadavbusiness1@gmail.com>
2026-01-21 17:56:19 +0530

    add renderr url

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -19,7 +19,9 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: ["http://localhost:3000","https://story-book-web-eight.vercel.app"], 
+    origin: ["http://localhost:3000",
+      "https://story-book-web-eight.vercel.app",
+      "https://story-book-backend.onrender.com"], 
 
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
